b8092cd8158791a084dcb9bab12a18d7ca8d5077
Sean Roberts <seanorama@users.noreply.github.com>
2018-04-20 12:36:46 +0100

    Add Tomahawk.app 0.8.4

---
+++ Casks/tomahawk.rb
@@ -0,0 +1,10 @@
+cask 'tomahawk' do
+  version '0.8.4'
+  sha256 '65fba8767e21b898a61feeb077ae9a87685b5ebf4d2ecbccddd7168e85e695da'
+
+  url "http://download.tomahawk-player.org/Tomahawk-#{version}.dmg"
+  name 'Tomahawk'
+  homepage 'https://www.tomahawk-player.org/'
+
+  app 'Tomahawk.app'
+end
